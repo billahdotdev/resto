ccc1e4df352b95f56e64800511f27d801b58eb31
Masum Billah <emasumbillah@gmail.com>
2025-03-07 23:30:24 +0600

    Fixed Tailwind & Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/resto/', // Change to your repo name
+  base: '/resto/', // Change this to match your GitHub repo name
   build: {
     outDir: 'dist', // Ensure the output folder is correct
-  }
+  },
 });
